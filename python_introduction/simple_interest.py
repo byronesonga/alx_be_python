f60cb40fb901b16e5791d5e4f8a9811dae5dd7b5
Byrone Songa <byronesonga68@gmail.com>
2024-09-15 00:27:03 +0300

    simple interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,10 +1,10 @@
-def I(P,R,T):
-    return P * R * T
+def interest(P,R,T):
+    I = P*R*T
+    return I
 P = 1000
 R =0.05
 T =3
 principal = 1000
 rate = 0.05 
 time = 3 
-intrest = P * R  * T
-print("The simple intrest is:", intrest)
+print("The simple intrest is:", interest(P,R,T))
