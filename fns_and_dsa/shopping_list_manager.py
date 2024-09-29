5dd12ec3757d86b7aeb2cbce63e006fe235287bd
Byrone Songa <byronesonga68@gmail.com>
2024-09-29 21:51:35 +0300

    lists

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,29 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = int(input("Enter your choice:"))
+        if choice ==1:
+            item = input("Enter the item to add:")
+            shopping_list.appenda(item)
+        elif choice ==2:
+            item = input("Enter the item to remove:")
+            if item in shopping_list:
+                shopping_list.remove(item)
+                print(f"{item} is")
+            else:
+                print(f"{item} is not found in the list")
+        elif choice ==3:
+            print(f"{shopping_list}")
+        elif choice ==4:
+            print("Goodbye!")
+        else:
+            print("Invalid choice. Please try again.")
+    if __name__ == "__main__":
+        main()
