ab50e5a3c499c6b26f68dba65e27cad379cd80d1
Byrone Songa <byronesonga68@gmail.com>
2024-09-29 21:06:56 +0300

    functions

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,17 +1,14 @@
 def perform_operation(num1, num2,operation):
     if operation == "add":
-        return num1+num2
+        return num1 + num2
     elif operation == "subtract":
-        return num1-num2
+        return num1 - num2
     elif operation == "multiply":
-        return num1*num2
+        return num1 * num2
     elif operation == "divide":
         if num2 == 0:
             return "Error:cannot divide by zero"
         else:
             return num1/num2
     else:
-        return "Error: Unsupported opeartion"
-
-result =perform_operation(num1,num2,operation)
-print(result)
+        return "Error: Unsupported operation"
