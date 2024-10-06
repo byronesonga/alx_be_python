8c699a2522cd7c2e63a64aeaf088fdc4fb40bf4a
Byrone Songa <byronesonga68@gmail.com>
2024-10-06 21:16:47 +0300

    unittest

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,8 +1,11 @@
-import unittest 
+import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
-    def test_add(self):
+    def setUp(self):
+        self.calculator = SimpleCalculator()
+
+    def test_addition(self):
         self.assertEqual(self.calculator.add(2, 3), 5)
         self.assertEqual(self.calculator.add(-1, 1), 0)
         self.assertEqual(self.calculator.add(0, 0), 0)
