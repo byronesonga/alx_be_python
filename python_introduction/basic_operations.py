c48971b996b500aa9f68a0059d0f1ad4f22d820b
Byrone Songa <byronesonga68@gmail.com>
2025-07-11 21:24:41 +0300

    basic operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,7 @@
+# assign values to the variables 
+number1 = 10 
+number2 = 5 
+
+print("Addition", "of", number1, "and", number2, "is", number2 + number1)
+print("Subtraction", "of", number1, "and", number2, "is", number1 - number2)
+print("Multiplication", "of", number1, "and", number2, "is", number1 * number2)
