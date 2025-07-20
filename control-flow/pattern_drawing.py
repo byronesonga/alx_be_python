b8e1268a2397c35c8ae49f5763c89f44a6aedf68
Byrone Songa <byronesonga68@gmail.com>
2025-07-20 05:23:36 +0300

    daily reminder

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,11 @@
+size = int(input("Enter the size of the pattern: "))
+if size > 0:
+    count = 0
+    while count < size: 
+        for i in range(size):
+            print("*" * size , end=" ")
+            print()
+            count +=1 
+        
+else:
+    print("Error: Please enter a positive integer other than 0")
