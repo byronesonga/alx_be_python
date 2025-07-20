132fbd2b44d5231e15e93716440150e830ac8865
Byrone Songa <byronesonga68@gmail.com>
2025-07-20 05:40:13 +0300

    daily reminder

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -2,7 +2,7 @@ size = int(input("Enter the size of the pattern: "))
 if size > 0:
     count = 0
     while count < size: 
-        for i in range(size):
+        for i in range(1, size+1):
             print(size * "*" , end=" ")
             print()
             count +=1 
