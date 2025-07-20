e03fe34a24eecb8b3e881331c24c937cdd12a983
Byrone Songa <byronesonga68@gmail.com>
2025-07-20 19:39:30 +0300

    pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -3,9 +3,9 @@ if size > 0:
     count = 0
     while count < size: 
         for i in range(1, size+1):
-            print(size * "*",end=" ")
-            print()
-            count +=1 
+            print("*", end=" ")
+        print()
+        count +=1 
         
 else:
     print("Error: Please enter a positive integer other than 0")
