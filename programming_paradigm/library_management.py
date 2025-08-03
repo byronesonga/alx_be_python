3812b35ccddc80aaf99e5ae4e394842a3c2fa5a0
Byrone Songa <byronesonga68@gmail.com>
2025-08-03 02:50:47 -0600

    library management

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,47 @@
+class Book:
+    def __init__(self, title, author, is_checked_out =False):
+        self.title = title 
+        self.author = author 
+        self._is_checked_out = is_checked_out
+
+
+    def check_out(self):
+        self._is_checked_out = True
+
+    def return_book(self):
+        self._is_checked_out = False
+
+    def is_available(self):
+        return not self._is_checked_out
+    
+    
+class Library:
+    def __init__(self):
+        self._books = []
+
+    def add_book(self, book):
+        self._books.append(book)
+        
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title.lower() == title.lower():
+                if book.is_available():
+                    book.check_out()
+        
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title.lower() == title.lower():
+                if not book.is_available():
+                    book.return_book()
+
+    def list_available_books(self):
+        available_books = [book for book in self._books if book.is_available()]
+        if available_books:
+            for book in available_books:
+                print(f'{book.title} by {book.author}')
+        
+    
+            
+        
