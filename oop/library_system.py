af4ddf20e0a28b1e463af5f495c4ab94ab9e0239
Byrone Songa <byronesonga68@gmail.com>
2025-08-09 11:49:27 +0300

    composition

--- oop/library_system.py
+++ oop/library_system.py
@@ -12,7 +12,7 @@ class EBook(Book):
         super().__init__(title, author)
         self.file_size = file_size
     def __str__(self):
-        return f"Ebook: {self.title} by , {self.author} File Size: {self.file_size}KB"  
+        return f"Ebook: {self.title} by {self.author}, File Size: {self.file_size}KB"  
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
@@ -24,9 +24,7 @@ class PrintBook(Book):
         
 class Library:
     def __init__(self, books=None):
-        if books is None:
-            books = []
-        self.books = books 
+            self.books = books or []
     
     def add_book(self, book):
         self.books.append(book)
